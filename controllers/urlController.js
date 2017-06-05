c132329bb0f1398504b7a6b3db4d7ebc5071603b
fhirt <fabio_hirt@hotmail.com>
2017-06-05 12:41:34 +0000

    added url verification

--- controllers/urlController.js
+++ controllers/urlController.js
@@ -1,4 +1,6 @@
-var URL = require('../models/urlModel');
+const URL = require('../models/urlModel');
+const isURL = require('is-url');
+const isReachable = require('is-reachable');
 
 exports.list = function(req, res, next) {
     URL.find({}, {'_id':0, tiny:1, url:1} , function(err, result) {
@@ -11,24 +13,29 @@ exports.list = function(req, res, next) {
 exports.add = function(req, res, next) {
     var newURL = req.params['url'] + req.params['0'];
     
-    URL.findOne({'url': newURL}, {url:1, tiny:1}, function(err, result) {
-        if (err) return next(err);
+    if (!isURL(newURL)) return res.send({'answer':'enter a valid url in the form: http(s)://someurl.com'});
+    isReachable(newURL).then(reachable => {
+        if (!reachable) return res.send({'answer':'the site at: ' + newURL + ' is not reachable'});
         
-        if (result) {
-            return res.send(result);
-        } else {
-            URL.count({}, function(err, count){
-                if (err) console.error(err);
-                var tinyURL = new URL({
-                    url: newURL,
-                    tiny: count
+        URL.findOne({'url': newURL}, {'_id':0, url:1, tiny:1}, function(err, result) {
+            if (err) return next(err);
+            
+            if (result) {
+                return res.send(result);
+            } else {
+                URL.count({}, function(err, count){
+                    if (err) console.error(err);
+                    var tinyURL = new URL({
+                        url: newURL,
+                        tiny: count
+                    });
+                    tinyURL.save(function(err) {
+                        if (err) return next(err);
+                        res.send({url: tinyURL.url, tiny: tinyURL.tiny});
+                    });
                 });
-                tinyURL.save(function(err) {
-                    if (err) return next(err);
-                    res.send({url: tinyURL.url, tiny: tinyURL.tiny});
-                });
-            });
-        }
+            }
+        });
     });
 };
 
